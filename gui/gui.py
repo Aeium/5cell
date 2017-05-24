d18bc7f2ce7c6d5949833c38b2f4613ef4118f31
Aeium <nate2@umbc.edu>
2017-05-24 14:34:54 -0400

    updates to gui.py

--- gui/gui.py
+++ gui/gui.py
@@ -1,17 +1,66 @@
 from __future__ import print_function
 
 from Tkinter import *
-from ctypes import windll
 from PIL import Image, ImageTk
 import numpy as np
 import random
+import sys
 from sys import stdout
 from time import sleep
 from bitstring import BitArray
+from ConfigParser import SafeConfigParser
+
+import grequests
 
 
+ABOUT_TEXT = """
+                          Welcome to Aeium's Map!
+
+                Each pixel in the map represents an automation.
+
+                                 Controls:
+
+               Place reticule          - Mouse click near pixel
+
+               Move reticule           - Arrow keys or 'WASD'
+
+         Execute Selected Automation   - SpaceBar
+
+       Save Selected Automation to png - ctrl+Spacebar
+
+    Tip: Position windows with windows key and arrows (on windows PC).
+
+    To see this prompt again, delete the settings.ini file then restart."""
+
+
+FEILD_1   = """                                  Settings:
+
+          How many characters per line do you want in the terminal?
+
+                               Please type a number"""
+
+FEILD_2   = """
+           Do you want to contribute data for a collaborative heatmap?
+
+                 heatmap URL: https://aeiums-map.herokuapp.com/
+
+           (You may need to permit outbound traffic in your firewall)
+
+                              Please enter 'y' or 'n'"""
+
+DISCLAIMER = """
+Contact:
+Twitter: https://twitter.com/Aeium
+Get in touch or follow for updates about this and other projects
+
+Disclaimer:
+This tool is provided as is. Aeium is not liable. Use at your own risk.
+"""
+
+	
+	
 root = Tk()
-
+root.title("Aeium's Map")
 
 rule = 0 #event.x/4 + (event.y /4) * 256
 
@@ -23,19 +72,103 @@ label = Label(root, image=display)
 label.pack()
 
 
-def printline(step):
+class configWindow(object):
+    def __init__(self,master):
+        top=self.top=Toplevel(master)
+        T1 = Text(top,height=19, bg='lightgrey', relief='flat')
+        T1.insert(END, ABOUT_TEXT)
+        T1.config(state=DISABLED)
+        T1.pack()
+        #self.l1=Label(top,text=ABOUT_TEXT)
+        #self.l1.pack()
+        T2 = Text(top, height=5, bg='lightgrey', relief='flat')
+        T2.insert(END, FEILD_1)
+        T2.config(state=DISABLED)
+        T2.pack()
+        #self.l2=Label(top,text=FEILD_1)
+        #self.l2.pack()
+        self.e1=Entry(top)
+        self.e1.insert(0,"116")
+        self.e1.pack()
+        T3 = Text(top, height=8, bg='lightgrey', relief='flat')
+        T3.insert(END, FEILD_2)
+        T3.config(state=DISABLED)
+        T3.pack()
+        #self.l3=Label(top,text=FEILD_2)
+        #self.l3.pack()
+        self.e2=Entry(top)
+        self.e2.insert(0,"y")
+        self.e2.pack()
+        T4 = Text(top, height=8, bg='lightgrey', relief='flat')
+        T4.insert(END, DISCLAIMER)
+        T4.config(state=DISABLED)
+        T4.pack()
+        #self.l4=Label(top,text=DISCLAIMER)
+        #self.l4.pack()
+        self.b=Button(top,text='Ok',command=self.cleanup)
+        self.b.pack()
+        Toplevel.focus_force(top)
+    def cleanup(self):
+        self.value= self.e1.get(),self.e2.get()
+        self.top.destroy()
+		
 
-    count = 0	
-    for i in step:	
-        if count > 16 and count < 116:
-            if (i == 1):
-                print ("X", end="")
-            else:
-                print (" ", end="")
-        count = count + 1
+		
+parser = SafeConfigParser()
+parser.read('settings.ini')
+
+settings = None
+
+try:
+    termWidth = int(parser.get("Settings", 'Terminal_Width'))
+    sendData  = parser.get("Settings", 'Send_Useage_Data')
+    settings  = True
+except:
+    settings = False
+	
+if (settings == False):
+    window = configWindow(root)
+
+    root.wait_window(window.top)
+	
+    termWidth = int(window.value[0])
+	
+    if (window.value[1][0].lower() == 'y'):
+        sendData = True
+    else:
+        sendData = False
+		
+    parser.add_section('Settings')
+    parser.set('Settings', 'Terminal_Width', str(termWidth))
+    parser.set('Settings', 'Send_Useage_Data',str(sendData))
+	
+    with open('settings.ini', 'w') as configfile:
+        parser.write(configfile)
 		
 		
 		
+def drawReticule(cursorPix, x, y):
+
+    cursorPix[x,y] = (255,0,0)
+
+    cursorPix[(x + 2)%256,y] = (255,0,0)		
+    cursorPix[(x + 3)%256,y] = (255,0,0)	
+    cursorPix[(x + 4)%256,y] = (255,0,0)	
+
+    cursorPix[(x - 2)%256,y] = (255,0,0)	
+    cursorPix[(x - 3)%256,y] = (255,0,0)	
+    cursorPix[(x - 4)%256,y] = (255,0,0)	
+	
+    cursorPix[x,(y + 2)%256] = (255,0,0)	
+    cursorPix[x,(y + 3)%256] = (255,0,0)
+    cursorPix[x,(y + 4)%256] = (255,0,0)
+	
+    cursorPix[x,(y - 2)%256] = (255,0,0)
+    cursorPix[x,(y - 3)%256] = (255,0,0)
+    cursorPix[x,(y - 4)%256] = (255,0,0)
+	
+    return cursorPix
+	
 def auto(ruleNum , length, printBool):
 
 
@@ -44,17 +177,20 @@ def auto(ruleNum , length, printBool):
     for element in init:
         init[count] = random.randint(0,1)
         if printBool and init[count] == 1:
-            print ("X", end="")
+            print (u"\u2588", end="")
         elif printBool:
             print (" ", end="")
         count = count + 1
 
+    if (not printBool):
+        image = np.zeros((length, length), dtype=np.uint8)
+		
     #if (printBool):
     #    print ("~~~~~~~~~~~~~~ %s ~~~~~~~~~~~~~~~~~" % ruleNum)
 
     rule = BitArray(uint=ruleNum, length=16)
 	
-    #rule = rule[::-1]
+    rule = rule[::-1]  # this is so the endian of the rule matches the code that create the image
 	
 	# 0101 0110 0110 0110
 
@@ -73,9 +209,14 @@ def auto(ruleNum , length, printBool):
     count = 0
 
     #for j in range(4000):
-    while count < (40): #newDelta < oldDelta:
+	
+    if (printBool):
+        limit = 40
+    else:
+        limit = length
+	
+    while (count < limit):
 
-        count = count + 1
         oldDelta = newDelta
 
         summation = 0 
@@ -105,7 +246,7 @@ def auto(ruleNum , length, printBool):
             #print direction
 
             if printBool and frame2[i] == 1:
-                print ("X", end="")
+                print (u"\u2588", end="")
             elif printBool:
                 print (" ", end="")
             #summation = summation + int(frame2[i])
@@ -121,14 +262,20 @@ def auto(ruleNum , length, printBool):
         #if printBool:
         #    print delta
 
+        if (not printBool):
+            image[count,:] = frame2
+		
+        count = count + 1
+		
         temp   = frame1
         frame1 = frame2
         frame2 = temp
 
-        #output_img[count,:] = frame1
-
-    return direction #output_img, direction #np.array_equal(frame1, first)
-		
+    if (printBool):
+        return direction
+    else:
+        return image
+        
 		
 def click(event):
 
@@ -137,13 +284,15 @@ def click(event):
     global label
     rule = event.x/4 + (event.y /4) * 256
 	
-    cursor                      = baseImage.copy()
-    cursorPix                   = cursor.load()
-    cursorPix[event.x/4,event.y/4] = (255,0,0)
-    cursor.save('%s.png' % rule)
-    display                     = ImageTk.PhotoImage(cursor.resize((2**10, 2**10)).convert("RGB"))
+    cursor       = baseImage.copy()
+    cursorPix    = cursor.load()
+    cursorPix    = drawReticule(cursorPix, event.x/4,event.y/4)
+    display      = ImageTk.PhotoImage(cursor.resize((2**10, 2**10)).convert("RGB"))
+	
     label.configure(image=display)
     label.image = display
+	
+    print ("X: %03d  Y: %03d  Rule: %05d" % (event.x/4, event.y/4, rule))
 
 def spaceKey(event):
 
@@ -156,24 +305,33 @@ def spaceKey(event):
 
     """
 
+    global termWidth
+    global sendData
 	
     lab= Label(root, text = 'X: %04d Y: %03d Rule: %05d' %(event.x, event.y, rule))
     lab.pack()
 	
     print (rule)
 	
-    direction = auto(rule, 116, True)
-
+    direction = auto(rule, termWidth, True)   # this actually runs the automation
+	
     x = rule %  256
     y = rule // 256
 		
-    print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    #print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	
-    print ("X: %03d  Y: %03d  Rule: %05d   Direction: %05d" % (x, y, rule, direction))
+    print ("\nX: %03d  Y: %03d  Rule: %05d   Direction: %05d" % (x, y, rule, direction))
+	
+    if (sendData == 'True'):    # evaluate this alkwardly because it's a string
+        urls = ['https://aeiums-map.herokuapp.com/userData']
+
+        params = {'rule':rule}
+        rs = (grequests.post(u, data=params) for u in urls)
+        grequests.map(rs)
+	
+    #if (sendData == 'True'):    # evaluate this alkwardly because it's a string
+    #    r = async.post('https://aeiums-map.herokuapp.com/userData', data = {'rule':rule})
     #"""
-
-
-root.bind('<Button-1>', click)
 
 
 def leftKey(event):
@@ -188,7 +346,7 @@ def leftKey(event):
 
     cursor         = baseImage.copy()
     cursorPix      = cursor.load()
-    cursorPix[x,y] = (255,0,0)
+    cursorPix      = drawReticule(cursorPix,x,y)
     display        = ImageTk.PhotoImage(cursor.resize((2**10, 2**10)).convert("RGB"))
     label.configure(image=display)
     label.image = display
@@ -207,7 +365,7 @@ def rightKey(event):
 	
     cursor         = baseImage.copy()
     cursorPix      = cursor.load()
-    cursorPix[x,y] = (255,0,0)
+    cursorPix      = drawReticule(cursorPix,x,y)
     display        = ImageTk.PhotoImage(cursor.resize((2**10, 2**10)).convert("RGB"))
     label.configure(image=display)
     label.image = display
@@ -226,7 +384,7 @@ def upKey(event):
 
     cursor         = baseImage.copy()
     cursorPix      = cursor.load()
-    cursorPix[x,y] = (255,0,0)
+    cursorPix      = drawReticule(cursorPix,x,y)
     display        = ImageTk.PhotoImage(cursor.resize((2**10, 2**10)).convert("RGB"))
     label.configure(image=display)
     label.image = display
@@ -245,18 +403,70 @@ def downKey(event):
 
     cursor         = baseImage.copy()
     cursorPix      = cursor.load()
-    cursorPix[x,y] = (255,0,0)
+    cursorPix      = drawReticule(cursorPix,x,y)
     display        = ImageTk.PhotoImage(cursor.resize((2**10, 2**10)).convert("RGB"))
     label.configure(image=display)
     label.image = display
 	
     print ("X: %03d  Y: %03d  Rule: %05d" % (x, y, rule))
 
+IMAGE_SETTINGS_1 = """Please enter the dimension of the image you want to save."""
 
-root.bind('<Left>', leftKey)
+IMAGE_SETTINGS_2 = """Please enter the filename. (.png will be added automatically)"""
+
+	
+class imageWindow(object):
+    def __init__(self,master):
+        top=self.top=Toplevel(master)
+        self.l1=Label(top,text=IMAGE_SETTINGS_1)
+        self.l1.pack()
+        self.e1=Entry(top)
+        self.e1.insert(10,"512")
+        self.e1.pack()
+        self.l2=Label(top,text=IMAGE_SETTINGS_2)
+        self.l2.pack()
+        self.e2=Entry(top)
+        self.e2.pack()
+        self.b=Button(top,text='Ok',command=self.cleanup)
+        self.b.pack()
+        Toplevel.focus_force(top)
+    def cleanup(self):
+        print("Processing image: '%s.png'" % self.e2.get())
+        binaryImageValues = auto(rule, int(self.e1.get()), False)
+        output_img = np.multiply(binaryImageValues, 255)
+        im = Image.fromarray( output_img.astype('uint8'))
+        im.save('%s.png' % self.e2.get())
+        if (sendData == 'True'):    # evaluate this alkwardly because it's a string
+            urls = ['https://aeiums-map.herokuapp.com/userDataIm']
+            params = {'rule':rule}
+            rs = (grequests.post(u, data=params) for u in urls)
+            grequests.map(rs)
+		
+        #if (sendData == 'True'):    # evaluate this alkwardly because it's a string
+        #    r = async.post('https://aeiums-map.herokuapp.com/userDataIm', data = {'rule':rule})
+        print("Done")
+        self.top.destroy()
+		
+def imagefunc(event):
+
+    global root
+	
+    window = imageWindow(root)
+
+    root.wait_window(window.top)
+
+root.bind('<Button-1>', click)
+	
+root.bind('<Left>' , leftKey)
+root.bind('<a>'    , leftKey)
 root.bind('<Right>', rightKey)
-root.bind('<Up>', upKey)
-root.bind('<Down>', downKey)
+root.bind('<d>'    , rightKey)
+root.bind('<Up>'   , upKey)
+root.bind('<w>'    , upKey)
+root.bind('<Down>' , downKey)
+root.bind('<s>'    , downKey)
+
+root.bind('<Control-KeyPress-space>'  , imagefunc)
 
 root.bind('<space>', spaceKey)
 
